2042dfc437d08a6389334bf3da15dd62a9d4fad8
paul santillan <paulggsch@gmail.com>
2024-02-05 13:58:37 -0500

    new files updated

---
+++ index.js
@@ -0,0 +1,19 @@
+function Home() {
+    const listItems = [1, 2, 3, 4]
+
+    const addIem = () => {
+        const variableA = 1;
+        const variableB = 3;
+        const variableC = 2;
+
+        return variableA*3 + variableC*variableB
+    }
+
+    return (
+        <><div>Home</div>
+        <p>hola mundo</p>
+        <button onClick={additem}/></>
+    )
+}
+
+export default Home
